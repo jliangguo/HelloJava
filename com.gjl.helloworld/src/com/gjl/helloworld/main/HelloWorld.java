8aacd404ff742b7ec62ce19580e26eba6ff6fb63
jliangguo <jliangguo@gmail.com>
2013-07-09 23:34:07 +0800

    问题单号：[N/A] 修改人员：[gjl] 修改说明：[修改HelloWorld。] 修改模块：[HelloJava]

---
+++ com.gjl.helloworld/src/com/gjl/helloworld/main/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.gjl.helloworld.main;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("This is the first project in Eclipse 4.3.");
+		System.out.println();
+	}
+}
